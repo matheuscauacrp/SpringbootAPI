4db7ad8aa18cd68269b29f88e90e516654a241df
Matheus Cauã Rocha Porto <matheuscrp1@hotmail.com>
2022-09-20 20:19:05 -0300

    Passando parametros

--- demo/src/main/java/springboot/restapi/controller/indexController.java
+++ demo/src/main/java/springboot/restapi/controller/indexController.java
@@ -4,14 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController/*Arquiterura REST*/
 @RequestMapping(value = "/usuario")
 public class indexController {
-	
+	/* SERVIÇO RESTFUL */
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity("Olá Usuário IndexController, Deu certo aqui!", HttpStatus.OK);
+	public ResponseEntity init(@RequestParam (value = "nome") String nome) {
+		System.out.println("Parametro " +nome);
+		return new ResponseEntity("Olá Usuário: "+nome+" IndexController, Deu certo aqui!", HttpStatus.OK);
 	}
 }
